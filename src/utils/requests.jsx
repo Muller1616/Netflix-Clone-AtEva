d709c68ca6206c9f17bd70082ceb6f2dcd4101cd
Muller1616 <mulugetaabeje16@gmail.com>
2025-12-10 12:43:01 +0300

    banner part

--- src/utils/requests.jsx
+++ src/utils/requests.jsx
@@ -1,4 +1,4 @@
-API_KEY = process.env.REACT_APP_API_KEY;
+const API_KEY = import.meta.env.VITE_API_KEY;
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
@@ -9,3 +9,5 @@ const requests = {
   fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
   fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
 };
+
+export default requests;
